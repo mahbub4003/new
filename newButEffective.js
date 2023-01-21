95c4c77e2a6b7f290cbd970a4355d7dcd9d3f813
mahbub-alam <mahbub01750934003@gmail.com>
2023-01-21 22:28:17 +0600

    second commit

--- newButEffective.js
+++ newButEffective.js
@@ -1,3 +1,9 @@
 const arr = Array.from(Array(4).keys())
 console.log(arr);
 // result [1,2,3,4]
+
+
+// git push in master branch
+
+//1.. git remote add origin https://github.com/mahbub4003/newButEffectiveData.git
+//2.. git push -u origin master
